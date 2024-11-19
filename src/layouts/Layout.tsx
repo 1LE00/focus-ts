c6f4e1a25113fcd0b049504d00b7a514e890fca1
Yogesh Dangal <ydangal5@gmail.com>
2024-11-19 21:09:08 +0545

    Added loading component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -36,13 +36,19 @@ export default function Layout() {
         }
     }, [db, dispatch])
 
-    return (status === 'succeeded' &&
-        <section
-            className={`focus-app bg-${darkTheme && isActive ? theme.dark : map[activeButton]
-                } min-h-screen p-3 flex flex-col items-center w-100 transition ease duration-1000`}
-        >
-            <Header />
-            <Outlet />
-        </section>
+    return (
+        (status === 'loading' ?
+            <section className={`bg-focus w-full h-dvh flex flex-col items-center justify-center`}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" className="size-24">
+                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+                </svg>
+                <p className="md:text-xl animate-pulse">Loading...</p>
+            </section> : <section
+                className={`focus-app bg-${darkTheme && isActive ? theme.dark : map[activeButton]
+                    } min-h-screen p-3 flex flex-col items-center w-100 transition ease-linear duration-500`}
+            >
+                <Header />
+                <Outlet />
+            </section>)
     );
 }
