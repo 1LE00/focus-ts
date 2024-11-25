09d21811335f30c42bbafa5940ea337d04e162fe
Yogesh Dangal <ydangal5@gmail.com>
2024-11-25 18:30:16 +0545

    updated task slice

--- src/features/tasks/taskSlice.ts
+++ src/features/tasks/taskSlice.ts
@@ -53,57 +53,7 @@ const initialState: TaskSlice = {
         name: '',
         color: ''
     },
-    tasks: [
-        {
-            id: "1",
-            name: "Complete report",
-            tags: [
-                { name: "Work", color: "#ff6347" }, // Tomato color
-                { name: "Urgent", color: "#ff4500" } // OrangeRed color
-            ],
-            project: { name: "Project A", color: "#1e90ff" }, // DodgerBlue color
-            isChecked: false,
-            pomodoroCount: 3,
-            note: "Remember to include the new financial data."
-        },
-        {
-            id: "2",
-            name: "Grocery shopping",
-            tags: [
-                { name: "Personal", color: "#32cd32" }, // LimeGreen color
-                { name: "Errand", color: "#ffa500" } // Orange color
-            ],
-            project: { name: "Home", color: "#6a5acd" }, // SlateBlue color
-            isChecked: false,
-            pomodoroCount: 1,
-            note: "Buy fruits, vegetables, and milk."
-        },
-        {
-            id: "3",
-            name: "Read a book",
-            tags: [
-                { name: "Leisure", color: "#ff69b4" }, // HotPink color
-                { name: "Reading", color: "#8a2be2" } // BlueViolet color
-            ],
-            project: { name: "Personal Development", color: "#20b2aa" }, // LightSeaGreen color
-            isChecked: false,
-            pomodoroCount: 2,
-            note: "Continue reading the second chapter."
-        },
-        {
-            id: "4",
-            name: "Workout",
-            tags: [
-                { name: "Health", color: "#00fa9a" }, // MediumSpringGreen color
-                { name: "Fitness", color: "#ff4500" } // OrangeRed color
-            ],
-            project: { name: "Wellness", color: "#ff1493" }, // DeepPink color
-            isChecked: false,
-            pomodoroCount: 1,
-            note: "Focus on cardio and core exercises."
-        }
-    ]
-
+    tasks: []
 };
 
 const taskSlice = createSlice({
